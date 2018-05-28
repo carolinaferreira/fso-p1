d06f2ab35a98b4f04c3765e460445cff49dee1b0
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2018-05-28 20:53:29 -0300

    Adding mutex init and destroy
    Co-authored-by: Matheus Oliveira <matheus_oliveira30@hotmail.com>
    Co-authored-by: carolinaferreira <carol.mf14@gmail.com>

--- 1.c
+++ 1.c
@@ -1,4 +1,7 @@
-/* Command line: gcc -Wall -ansi -lpthread 1.c */
+/* 
+    1: gcc -Wall -ansi -lpthread 1.c
+    2: ./a.out test1.txt
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -10,7 +13,9 @@
 extern int errno;
 
 unsigned short GRID[GRID_H][GRID_W];
+unsigned short SUM_THREAD = 0;
 pthread_mutex_t GRID_MUTEX[GRID_H][GRID_W];
+pthread_mutex_t SUM_THREAD_MUTEX;
 
 void print_grid(){
     int height, width;
@@ -37,8 +42,30 @@ void load_grid(FILE * file){
     }
 }
 
+void init_grid_mutex(){
+    int height, width;
+
+    for(height = 0; height < GRID_H; height++){
+        for(width = 0; width < GRID_W; width++){
+            pthread_mutex_init(&GRID_MUTEX[height][width], NULL);
+        }
+    }
+}
+
+void destroy_grid_mutex(){
+    int height, width;
+
+    for(height = 0; height < GRID_H; height++){
+        for(width = 0; width < GRID_W; width++){
+            pthread_mutex_destroy(&GRID_MUTEX[height][width]);
+        }
+    }
+}
+
 int main(int argc, char * argv[]){
     FILE * file_arg = NULL;
+
+    /* Opening file */
     file_arg = fopen(argv[1], "r+");
 
     printf("%s\n", argv[1]);
@@ -49,6 +76,12 @@ int main(int argc, char * argv[]){
     } else {
         perror("Aconteceu um erro");
     }
+
+    /* Initializing mutex */
+    init_grid_mutex();
+    destroy_grid_mutex();
+    pthread_mutex_init(&SUM_THREAD_MUTEX, NULL);
+    pthread_mutex_destroy(&SUM_THREAD_MUTEX);
 
     fclose(file_arg);
     return 0;
