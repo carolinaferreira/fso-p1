29ad1894b56b0ed823261ee4443f583f6f3f6f28
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2018-06-14 11:28:34 -0300

    Reading bits
    Co-authored-by: carolinaferreira <carol.mf14@gmail.com>
    Co-authored-by: Matheus Oliveira <matheus_oliveira30@hotmail.com>

--- projeto_2/manager.c
+++ projeto_2/manager.c
@@ -6,21 +6,42 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define BACKING_STORAGE_FILE "BACKING_STORE.bin"
 #define FRAME_SIZE 256
 #define FRAME_COUNT 256
 #define BYTE_LIMIT (FRAME_COUNT * FRAME_SIZE)
 
-void map_adresses(FILE * adresses){
-    while(!feof(adresses)){
-        int adr = 0;
+void showbits(unsigned int x)
+{
+    int i; 
+    for(i=(sizeof(int)*8)-1; i>=0; i--)
+            (x&(1u<<i))?putchar('1'):putchar('0');
+    
+    printf("\n");
+}
 
-        fscanf(adresses, "%d", &adr);
-        printf("Li: %d\n", adr);
+void map_addresses(FILE * addresses){
+    while(!feof(addresses)){
+        unsigned int adr = 0;
+
+        fscanf(addresses, "%u", &adr);
+        printf("Lendo endereço do arquivo: %u - ", adr);
+        showbits(adr);
+        
+        int id_page = adr >> 8;
+        printf("Id da pagina: %d - ", id_page);
+        showbits(id_page);
+        
+        int offset_page = (adr & 0xFF);
+        printf("Offset da pagina: %d - ", offset_page);
+        showbits(offset_page);
     }
 }
 
+
 int main(int argc, char *argv[]){
-    FILE * file_adresses = NULL;
+    FILE * file_addresses = NULL;
+    FILE * file_storage = NULL;
 
     /* If there isn't a file as argument */
     if(argc != 2){
@@ -29,17 +50,19 @@ int main(int argc, char *argv[]){
     }
 
     /* Opening file */
-    file_adresses = fopen(argv[1], "r+");
+    file_addresses = fopen(argv[1], "r+");
+    file_storage = fopen(BACKING_STORAGE_FILE, "r+");
 
     /* Checking for opening error */
-    if(file_adresses == NULL){
+    if(file_addresses == NULL || file_storage == NULL){
         perror("Aconteceu um erro");
         return -1;
     }
 
     /* Reading file */
-    map_adresses(file_adresses);
+    map_addresses(file_addresses);
 
-    fclose(file_adresses);
+    fclose(file_addresses);
+    fclose(file_storage);
     return 0;
 }
